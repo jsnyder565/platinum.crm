3660798301a591f38c32fef4759c71f3243a92ae
jsnyder565 <jsnyder.laravel@gmail.com>
2024-11-20 18:45:48 -0500

    Create CreateCustomers.php

---
+++ gist/CreateCustomers.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriberTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+          $table->id();
+          $table->string('name');
+          $table->string('email');
+          $table->string('phone');
+          $table->timestamps();
+        });
+    }
+}
