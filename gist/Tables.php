290ff10cd95879cc3bdbe8b24512d6a6d883d27f
jsnyder565 <jsnyder.laravel@gmail.com>
2024-11-20 19:34:32 -0500

    Update Tables.php

--- gist/Tables.php
+++ gist/Tables.php
@@ -1,1 +1,28 @@
+<?php
 
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomers extends Migration
+{
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+          $table->id();
+          $table->string('name');
+          $table->string('email');
+          $table->string('phone');
+          $table->timestamps();
+        });
+      Schema::create('purchases', function (Blueprint $table) {
+          $table->id();
+          $table->string('customer_email');
+          $table->string('purchasable');
+          $table->double('price');
+          $table->double('quantity');
+          $table->double('total');
+          $table->datetime('date');
+          $table->timestamps();
+        });
+    }
+}
