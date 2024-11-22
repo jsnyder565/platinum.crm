869a246f038f759761b9cc6eaa3417a4e52c30ec
jsnyder565 <jsnyder.laravel@gmail.com>
2024-11-21 21:35:59 -0500

    fixed bug in row pregeneration

--- data/generate_rows.sh
+++ data/generate_rows.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./parse_purchases.sh purchase_history.csv > purchases.php
 ./parse_customers.sh customers.csv > customers.php  
-cat customers.php purchases.php > rows.php
+cat rows.header.php customers.php purchases.php > rows.php
---
+++ data/rows.header.php
@@ -0,0 +1,4 @@
+<?php
+
+use App\Models\Customer;
+use App\Models\Purchase;
