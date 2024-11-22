e4c3187c18a7b0f6652949ed251eedaa5daca436
jsnyder565 <jsnyder.laravel@gmail.com>
2024-11-22 15:07:55 -0500

    add comments, remove redundant class definitions

--- laravel/crm/app/Models/Purchase.php
+++ laravel/crm/app/Models/Purchase.php
@@ -4,11 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Customer extends Model
-{
-  protected $fillable = ['name','email','phone_number','loyalty_points'];
-}
-
 class Purchase extends Model
 {
   protected $fillable = ['customer_id','purchasable','price','quantity','total','purchase_date','purchase_date_string'];
